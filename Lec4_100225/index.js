ab53d2b621a153d632e38a2313263d87185b1706
Mridul <mridulwadhwaa@gmail.com>
2025-02-10 15:39:36 +0530

    10 Feb 25

---
+++ Lec4_100225/index.js
@@ -0,0 +1,32 @@
+console.log("Hello");
+
+console.log("Start");
+
+var a = 10;
+
+function squareOfTwoNumber(x){
+    var ans = x*x;
+    console.log(ans);
+}
+
+console.log(squareOfTwoNumber(a));
+console.log("End");
+
+console.log(abc);
+console.log(sumOfTwo(10,5));
+var abc = 29;
+function sumOfTwo(a,b){
+    var ans =a+b;
+    return ans;
+}
+var majorAns = sumOfTwo(6,8);
+console.log(majorAns);
+
+console.log(xyz);
+console.log(sumOfTwo(10,16));
+let xyz = 12;
+function sumOfTwo(a,b){
+    let ans =a+b;
+    return ans;
+}
+console.log(sumOfTwo(19,27));
